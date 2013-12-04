69d2c97bfcc200adf28ad923a7ae7b9d527b1f4a
Kirk Graham <kegraham@containerstore.com>
2013-12-04 10:19:43 -0600

    Initial commit of my Rails depot application.

---
+++ app/models/product.rb
@@ -0,0 +1,41 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  description :text
+#  image_url   :string(255)
+#  price       :decimal(8, 2)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
+class Product < ActiveRecord::Base
+	has_many :line_items
+
+	before_destroy :ensure_not_referenced_by_any_line_item
+
+	validates :title, :description, :image_url, presence: true 
+	validates :price, numericality: {greater_than_or_equal_to: 0.01} 
+	validates :title, uniqueness: true, length: {minimum: 10}
+	validates :image_url, allow_blank: true, format: {
+    	with:    %r{\.(gif|jpg|png)\Z}i,
+		message: 'must be a URL for GIF, JPG or PNG image.' 
+	}
+
+	def self.latest
+		Product.order(:updated_at).latest
+	end
+
+	private
+		# ensure that there are no line items referencing this product
+		def ensure_not_referenced_by_any_line_item
+			if line_items.empty?
+				return true
+			else
+				errors.add(:base, 'Line Items present')
+				return false
+			end
+		end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '46d8968a587f7aa5fcaf0a23df7a6c9f2ffa3c0cff1388adcada429a8acace023993d7ba7f953cd542ce2bbf8b06f2b8a107af31dfe4a70a7fe5b8f0a157aa1b'
